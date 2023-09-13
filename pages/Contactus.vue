660cec5d74c72b35fad48cc502cf1ef622431038
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2023-09-14 04:00:27 +0530

    Update Contactus.vue

--- pages/Contactus.vue
+++ pages/Contactus.vue
@@ -2,7 +2,37 @@
 <template>
     <div>
         <Navbar />
-        contact us
+        
+        <div class="bg-slate-300 text-center block px-48 " id="bg"  >
+
+            <div class=" my-auto flex-auto w-full h-full py-12 text-center px-16  ">
+                <div class="h-48 flex-auto text-8xl ">
+                    <h1 class="my-auto">
+                        Contact Us
+                    </h1>
+                </div>
+                <p>Discover seamless communication on our Contact Us page. Connect
+                    with <span class="mecha">Mecha<span class="Verse">Verse</span></span> effortlessly – share inquiries,
+                    feedback, or simply say hello. We're here to engage and assist you!</p>
+                <br>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfv5f_eqZapAyx4vqKIosMscbnj52LlL39ztYmUOW9qj7G5Eg/viewform"
+                    target="_blank " class="contact">Reach Out to Us
+                </a>
+            </div>
+
+        </div>
+
         <Footer />
     </div>
 </template>
+<style>
+
+
+#bg{
+    background-image: url('../static/images/background8.jpg');
+    background-size: cover;
+    height: 500px;
+    background-position: center center;
+
+}
+</style>
