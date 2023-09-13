686e6ece97aa0a202b7d67206049fc8d94b73d6d
vivekrai00 <rai.vivek9900@gmail.com>
2023-09-14 01:37:27 +0530

    Update Navbar.vue

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- navbar here -->
+    <nav class="w-">
+        <div>
+        <div>
+            
+        </div>
+        <div>
+            mechaverse
+        </div>
+        <div>
+            <ul>
+                <li>Announcement</li>
+                <li>People</li>
+                <li>Activities</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div> 
+            tu logo
+        </div>
+    </div>
+    </nav>
+</template>
