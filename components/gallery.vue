0e8a30455dcefb227b5b7f0d237fa6b698cc2cb1
RAJVEER CHOUDHARY <rajveerjaat7300@gmail.com>
2023-09-14 01:41:07 +0530

    a

--- components/gallery.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        hello pussy
-    </div>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <!-- <gallery /> -->
     <Footer />
+    <Navbar />
   </div>
 </template>
 
