aee9b5b00a83c54750011042e8ebe257d97564ea
vivekrai00 <rai.vivek9900@gmail.com>
2023-09-16 03:05:36 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -145,12 +145,12 @@
 
         <!-- events cards -->
 
-        <div class="flex flex-row grid grid-rows-4">
-          <div>
-          <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+        <div class="  mb-10 space-x-5 flex justify-between">
+          <div class="w-[250px] h-[713px] text-center flex justify-center ">
+          <div class=" inline-block max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full h-[250px]" src="\images\emerging trends.jpeg" alt="Emerging trends in technology">
             <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <div class="font-bold text-xl mb-2">Emerging trends in technology</div>
               <p class="text-gray-700 text-base">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
                 eaque, exercitationem praesentium nihil.
@@ -166,11 +166,11 @@
             </div>
           </div>
           </div>
-          <div>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+          <div class="w-[250px] h-[713px]">
+            <div class=" inline-block max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full h-[250px]" src="\images\eng simulation.jpeg" alt="Engineerig simulation">
             <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <div class="font-bold text-xl mb-2">Exposoure of Engineerig Simulation </div>
               <p class="text-gray-700 text-base">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
                 eaque, exercitationem praesentium nihil.
@@ -186,30 +186,11 @@
             </div>
           </div>
           </div>
-          <div>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+          <div class="w-[250px] h-[713px]">
+            <div class="inline-block max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full h-[250px]" src="\images\fem.jpeg" alt="FEm">
             <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p class="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
-                eaque, exercitationem praesentium nihil.
-              </p>
-            </div>
-            <div class="px-6 pt-4 pb-2">
-              <span
-                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-              <span
-                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-              <span
-                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-            </div>
-          </div>
-          <div>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <div class="font-bold text-xl mb-2">Introduction to FEM</div>
               <p class="text-gray-700 text-base">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
                 eaque, exercitationem praesentium nihil.
@@ -225,7 +206,27 @@
             </div>
           </div>
           </div>
+          <div class="w-[250px] h-[713px]">
+            <div class="inline-block max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full h-[250]" src="\images\cfd.webp" alt="CFD">
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">Introduction to Computational Fluid Dynamics</div>
+              <p class="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
+                eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+              <span
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+              <span
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+              <span
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
           </div>
+          </div>
+          <!-- </div> -->
         </div>
         <Footer />
       </div>
